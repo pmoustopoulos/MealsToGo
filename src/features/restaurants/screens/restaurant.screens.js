4f82c0d87f9f401ee83aefcf76a4f58712c151af
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2023-02-12 19:54:39 +0200

    error after adding styled components

---
+++ src/features/restaurants/screens/restaurant.screens.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+export const RestaurantsScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar placeholder="Search" />
+    </View>
+    <View style={styles.list}>
+      <RestaurantInfoCard />
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
