e331590cae9a128e6026b0545dae3b36aaaaa2f1
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2023-02-18 08:38:27 +0200

    fix issue of react-native-safe-area-context

--- src/features/restaurants/screens/restaurant.screens.js
+++ src/features/restaurants/screens/restaurant.screens.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import SafeAreaView from "react-native-safe-area-view";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-      <Searchbar placeholder="Search" />
-    </View>
-    <View style={styles.list}>
-      <RestaurantInfoCard />
-    </View>
-  </SafeAreaView>
-);
+export const RestaurantsScreen = () => {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar placeholder="Search" />
+        </View>
+        <View style={styles.list}>
+          <RestaurantInfoCard />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
