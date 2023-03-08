13d99dee32205d124733dfc94865048575fb2f59
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2023-03-08 07:38:41 +0200

    add mocks

---
+++ src/services/restaurants/restaurants.service.js
@@ -0,0 +1,28 @@
+import { mocks } from "./mock";
+import camelize from "camelize";
+
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+
+    if (!mock) {
+      reject("not found");
+    }
+
+    resolve(mock);
+  });
+};
+
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
+
+restaurantsRequest()
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(JSON.stringify(transformedResponse, null, 3));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
